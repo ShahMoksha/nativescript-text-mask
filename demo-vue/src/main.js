f5563e2311a4197816fcc5b3298c4f90bc798c0a
Raj Bhatt <rjbhatt110@gmail.com>
2021-05-11 20:13:22 +0530

    Add files via upload

---
+++ demo-vue/src/main.js
@@ -0,0 +1,16 @@
+import Vue from 'nativescript-vue'
+import App from './components/App'
+
+
+// Prints Vue logs when --env.production is NOT set while building
+Vue.config.silent = (TNS_ENV === 'production')
+
+// Vue.registerElement(
+//   "MaskedTextField",
+//   () => require("nativescript-masked-text-field").MaskedTextField
+// );
+
+new Vue({
+  
+  render: h => h('frame', [h(App)])
+}).$start()
